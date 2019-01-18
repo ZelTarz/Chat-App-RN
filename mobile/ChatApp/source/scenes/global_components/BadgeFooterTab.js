46e1403ebc979796d1d2d3029dcf1d4edd2056eb
ZelTarz <15520688@gm.uit.edu.vn>
2019-01-18 19:39:02 +0700

    Fix bug with Android back button behavior on FooterTab: navigate but don't change active tab

--- mobile/ChatApp/source/scenes/global_components/BadgeFooterTab.js
+++ mobile/ChatApp/source/scenes/global_components/BadgeFooterTab.js
@@ -5,46 +5,32 @@ export default class BadgeFooterTabs extends Component {
     constructor(props){
         super(props);
         this.state = {
-            recentChatTab: true,
-            contactTab: false,
-            groupTab: false,
             recentChatBadge: 0,
             contactBadge: 0,
             groupBadge: 0,
         };
+
     };
 
     toogleRecentChatTab(){
-        this.setState({
-            recentChatTab: true,
-            contactTab: false,
-            groupTab: false
-        });
 
         const {navigate} = this.props.navigation
         navigate("RecentChat");
+
     };
 
     tooglecontactTab(){
-        this.setState({
-            recentChatTab: false,
-            contactTab: true,
-            groupTab: false
-        });
 
         const {navigate} = this.props.navigation
         navigate("Contacts");
+
     };
 
     tooglegroupTab(){
-        this.setState({
-            recentChatTab: false,
-            contactTab: false,
-            groupTab: true
-        });
 
         const {navigate} = this.props.navigation
         navigate("Groups");
+
     };
 
     render() {
@@ -52,33 +38,33 @@ export default class BadgeFooterTabs extends Component {
         <Footer>
           <FooterTab style = {{backgroundColor: 'white'}}>
             <Button 
-              active={this.state.recentChatTab}
+              active={this.props.navigation.state.index === 0}
               onPress={() => this.toogleRecentChatTab()}
               badge 
               vertical>
               <Badge><Text>{this.state.recentChatBadge}</Text></Badge>
-              <Icon active={this.state.recentChatTab} type="MaterialIcons" name="chat" />
-              {this.state.recentChatTab && <Text>Messages</Text>}
+              <Icon active={this.props.navigation.state.index === 0} type="MaterialIcons" name="chat" />
+              {this.props.navigation.state.index === 0 && <Text>Messages</Text>}
             </Button>
 
             <Button
-              active={this.state.contactTab}
+              active={this.props.navigation.state.index === 1}
               onPress={() => this.tooglecontactTab()}
               badge
               vertical>
               <Badge ><Text>{this.state.contactBadge}</Text></Badge>
-              <Icon active={this.state.contactTab} type="MaterialIcons" name="contacts" />
-              {this.state.contactTab && <Text>Contact</Text>}
+              <Icon active={this.props.navigation.state.index === 1} type="MaterialIcons" name="contacts" />
+              {this.props.navigation.state.index === 1 && <Text>Contact</Text>}
             </Button>
 
             <Button 
-              active={this.state.groupTab}
+              active={this.props.navigation.state.index === 2}
               onPress={() => this.tooglegroupTab()} 
               badge 
               vertical>
               <Badge ><Text>{this.state.groupBadge}</Text></Badge>
-              <Icon active={this.state.groupTab} type="FontAwesome" name="group" />
-              {this.state.groupTab && <Text>Group</Text>}
+              <Icon active={this.props.navigation.state.index === 2} type="FontAwesome" name="group" />
+              {this.props.navigation.state.index === 2 && <Text>Group</Text>}
             </Button>
 
           </FooterTab>
